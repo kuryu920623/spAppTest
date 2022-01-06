a9c5ec26cbf7a416643b1670006db030366c9b59
sora <sora@sora.com>
2022-01-06 18:17:19 +0900

    screen を切り出す

--- App.jsx
+++ App.jsx
@@ -1,37 +1,8 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps';
-import {
-  StyleSheet, Text, View, Dimensions,
-} from 'react-native';
-
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <View>
-        <MapView style={styles.map} />
-      </View> */}
-
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-
-    </View>
+    <MemoListScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
