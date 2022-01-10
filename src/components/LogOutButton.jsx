6b0811b07392c984136f0fd735f1e5110942880b
sora <sora@sora.com>
2022-01-10 10:18:23 +0900

    ログアウト機能追加

---
+++ src/components/LogOutButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import firebase from 'firebase';
+import {
+  StyleSheet, Text, TouchableOpacity, Alert,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function LogOutButton() {
+  const navigation = useNavigation();
+
+  function handlePress() {
+    firebase.auth().signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'LogIn' }],
+        });
+      })
+      .catch(() => {
+        Alert.alert('ログアウトに失敗しました。');
+      });
+  }
+  return (
+    <TouchableOpacity onPress={handlePress} style={styles.container}>
+      <Text style={styles.label}>ログアウト</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  label: {
+    fontSize: 14,
+    color: 'rgba(255, 255, 255, 0.7)',
+  },
+});
