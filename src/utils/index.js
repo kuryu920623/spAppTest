8b44636cb861f93c0571fa2f9c3976e402c0a796
sora <sora@sora.com>
2022-01-13 17:01:09 +0900

    エラー翻訳(途中)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,17 @@
 import { format } from 'date-fns';
 
 export function dateToString(date) {
-  if ( !date ) { return ''; }
+  if (!date) { return ''; }
   return format(date, 'yyyy年M月d日 HH時mm分');
+}
+
+export function translateErrors(code) {
+  const error = { title: 'エラー', descriprtion: '時間をおいてお試しください。' };
+  switch (code) {
+    case 'auth/invalid-email':
+      error.descriprtion = 'メールアドレスが不正です。';
+      break;
+    default:
+  }
+  return error;
 }
